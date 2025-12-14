01d1b0942782b5d84eec3a2f5264d19377f1fed9
MoniqueHinds <monique.hindss@gmail.com>
2025-12-14 00:45:07 -0500

    search bar feature on 3 main pages

---
+++ backendjs/search-bar.js
@@ -0,0 +1,48 @@
+// getting the search input box
+const searchInput = document.querySelector(".search-input");
+
+// getting the div where the results will be 
+const resultsContainer = document.querySelector(".search-results");
+
+// Only run if input exists
+if (searchInput && resultsContainer) {
+
+  // fetching all recipes from json file
+  let recipes = [];
+  fetch('../backendjs/recipes.json')
+  // converting json to an onject
+    .then(res => res.json())
+    // saving in variable called recipes
+    .then(data => recipes = data)
+    
+
+  // will listen for typing from user
+  searchInput.addEventListener('input', function() {
+
+    // making it so user doesnt have to use proper case
+    const query = this.value.toLowerCase();
+
+    // will clear the search and stop when erased 
+    resultsContainer.innerHTML = '';
+    if (query.length === 0) return;
+
+    // filtering to find recipes that match
+    const filtered = recipes.filter(r => r.name.toLowerCase().includes(query));
+
+    // Show matching recipes
+    filtered.forEach(r => {
+        //linking to recipe page
+      const card = document.createElement('a');
+      card.href = r.url;
+      //styyling like card
+      card.className = 'recipe-card';
+      //using image and name from json file
+      card.innerHTML = `
+        <img src="${r.image}" alt="${r.name}">
+        <p>${r.name}</p>
+      `;
+      //adding results to page
+      resultsContainer.appendChild(card); 
+    });
+  });
+}
